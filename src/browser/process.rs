aceee5ccf32c3b05c042d1d2318e1a1956a9cf35
Jeremiah Russell <jrussell@jerus.ie>
2020-09-01 11:37:59 +0100

    Support for use in AWS lambda functions
    Default configuration times out in AWS Lambda functions
    Solution
     * Add a feature flag "lambda" to configure required changes
     * Add arguments for lambda behind feature flag
     * Flag default features for not(lambda)
    
    Note
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
        modified:   Cargo.toml
        modified:   src/browser/process.rs

--- src/browser/process.rs
+++ src/browser/process.rs
@@ -114,8 +114,51 @@ impl<'a> LaunchOptions<'a> {
     }
 }
 
+/// These are passed to the Chrome binary if the lambda features is enabled.
+/// Via https://github.com/alixaxel/chrome-aws-lambda/blob/b06eaa1e1e6f060bbd52c6a7f1abab2e105784c1/source/index.js#L83
+#[cfg(feature = "lambda")]
+static DEFAULT_ARGS: [&str; 23] = [
+    "--autoplay-policy=user-gesture-required",
+    "--disable-background-networking",
+    "--disable-background-timer-throttling",
+    "--disable-backgrounding-occluded-windows",
+    "--disable-breakpad",
+    "--disable-client-side-phishing-detection",
+    "--disable-component-update",
+    "--disable-default-apps",
+    "--disable-dev-shm-usage",
+    "--disable-domain-reliability",
+    "--disable-extensions",
+    "--disable-features=AudioServiceOutOfProcess",
+    "--disable-hang-monitor",
+    "--disable-ipc-flooding-protection",
+    "--disable-notifications",
+    "--disable-offer-store-unmasked-wallet-cards",
+    "--disable-popup-blocking",
+    "--disable-print-preview",
+    "--disable-prompt-on-repost",
+    "--disable-renderer-backgrounding",
+    "--disable-setuid-sandbox",
+    "--disable-speech-api",
+    "--disable-sync",
+    "--disk-cache-size=33554432",
+    "--hide-scrollbars",
+    "--ignore-gpu-blacklist",
+    "--metrics-recording-only",
+    "--mute-audio",
+    "--no-default-browser-check",
+    "--no-first-run",
+    "--no-pings",
+    "--no-sandbox",
+    "--no-zygote",
+    "--password-store=basic",
+    "--use-gl=swiftshader",
+    "--use-mock-keychain",
+];
+
 /// These are passed to the Chrome binary by default.
 /// Via https://github.com/GoogleChrome/puppeteer/blob/master/lib/Launcher.js#L38
+#[cfg(not(feature = "lambda"))]
 static DEFAULT_ARGS: [&str; 23] = [
     "--disable-background-networking",
     "--enable-features=NetworkService,NetworkServiceInProcess",
